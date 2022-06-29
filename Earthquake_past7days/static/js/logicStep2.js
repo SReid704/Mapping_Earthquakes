d9cd43ffba6f94c1f75578b1e830075ab612ba49
apple <apple@Sha.lan>
2022-06-28 21:16:55 -0400

    Updated logicStep2

--- Earthquake_past7days/static/js/logicStep2.js
+++ Earthquake_past7days/static/js/logicStep2.js
@@ -35,7 +35,7 @@ L.control.layers(baseMaps).addTo(map);
 // This function returns the style data for each of the earthquakes we plot on
 // the map. We pass the magnitude of the earthquake into a function
 // to calculate the radius.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
   function styleInfo(feature) {
     return {
       opacity: 1,
@@ -46,7 +46,7 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       stroke: true,
       weight: 0.5
     }
-  })
+  }
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
   function getRadius(magnitude) {
@@ -67,3 +67,4 @@ pointToLayer: function(feature, latlng) {
         // We set the style for each circleMarker using our styleInfo function.
       style: styleInfo
       }).addTo(map);
+});
